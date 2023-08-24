bfc40cf85825114240dce52389641a88f0603fd6
aidan mulvaney <65428883+addieoG@users.noreply.github.com>
2023-08-24 19:50:47 +0100

    add js functions

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+function runGame() {
+
+}
+
+function checkAnswer(){
+
+}
+
+function calculateCorrectAnswer(){
+
+}
+
+function incrimentScore() {
+
+}
+
+function incrimentWrongAnswer(){
+
+}
+
+function displayAdditionQuestion(){
+
+}
+
+function displaySubtractQuestion(){
+
+}
+
+function displayMultiplyQuestion(){
+    
+}
